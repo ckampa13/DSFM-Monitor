044dcc711b70e442adf7a39118e7a036c72f9781
Lszemraj <83932124+Lszemraj@users.noreply.github.com>
2021-07-15 14:16:22 -0500

    Fixed ImagePlotDash.py to display image and removed x and y axis

--- Development/ImagePlotDash.py
+++ Development/ImagePlotDash.py
@@ -4,6 +4,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.io as pio
+import base64
+
 
 #attempting to display propeller image as background image in plot
 
@@ -15,6 +17,11 @@ import plotly.io as pio
 # #add the prefix that plotly will want when using the string as source
 # encoded_image = "data:image/png;base64," + encoded_string
 
+image_filename = '/Users/Lillie/Desktop/Mu2e/DSFMimage1.png'
+encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+
+
+
 fig = go.Figure()
 
 # Add trace
@@ -25,24 +32,38 @@ fig.add_trace(
 # Add images
 fig.add_layout_image(
         dict(
-            source=Image.open("/Users/Lillie/Desktop/Mu2e/DSFMimage1.png"),
+            source= 'data:image/png;base64,{}'.format(encoded_image.decode()),
             xref="x",
             yref="y",
             x=0,
             y=3,
             sizex=2,
-            sizey=2,
+            sizey=4,
             sizing="stretch",
-            opacity=0,
+            opacity=0.7,
             layer="below")
 
 )
 
+#x axis turn off
+fig.update_xaxes(visible=False)
+
+#y axis turn off
+fig.update_yaxes(visible=False)
+
+#Legend (not included in this plot)
+#fig.update_layout(showlegend=False)
+
+
+
+#Write image
 pio.write_image(fig, 'fig1.png')
+
 # Set templates
 fig.update_layout(template="plotly_white")
 
 
+#Dash app
 app = dash.Dash()
 app.layout = html.Div([
     dcc.Graph(figure=fig)
