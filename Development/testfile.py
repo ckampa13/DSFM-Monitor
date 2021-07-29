4ac1295b2fa78dea6a50ecf6e6015d2c8c8fc224
Lszemraj <83932124+Lszemraj@users.noreply.github.com>
2021-07-28 23:36:13 -0500

    Added rounding and time delay

--- Development/testfile.py
+++ Development/testfile.py
@@ -4,7 +4,11 @@ import numpy as np
 from nptdms import TdmsWriter, GroupObject, ChannelObject
 
 with TdmsFile.open("../data/TestDataV2.tdms") as tdms_file:
-    all_groups = tdms_file.groups()
+
+    #all_groups = tdms_file.groups()
+    for group in tdms_file.groups():
+        print(group)
     #print(all_groups)
-    print(tdms_file["step:1.1.4"]["HallProbes"][:])
-    #print(tdms_file["Step:1.1.4"]["Mapper"][:])
+
+    #print(tdms_file["step:1.1.4"]["Measured Coordinates"][:])
+    #print(tdms_file["step:1.1.11"]["HallProbes"][:])
