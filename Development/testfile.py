9e259fb234df34f85cfed9c32362432a544a9c97
Lszemraj <83932124+Lszemraj@users.noreply.github.com>
2021-08-02 13:06:59 -0500

    Converting TDMS to pandas df

--- Development/testfile.py
+++ Development/testfile.py
@@ -9,6 +9,6 @@ with TdmsFile.open("../data/TestDataV2.tdms") as tdms_file:
     for group in tdms_file.groups():
         print(group)
     #print(all_groups)
-
     #print(tdms_file["step:1.1.4"]["Measured Coordinates"][:])
-    #print(tdms_file["step:1.1.11"]["HallProbes"][:])
+    print(tdms_file["step:1.2.11"].channels())
+    print(tdms_file["step:1.2.11"]['Current'][:])
